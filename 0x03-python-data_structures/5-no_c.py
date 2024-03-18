4aac82498ea91d38c6192c5eccc4b2e071ff17d3
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-03-18 12:20:28 -0700

    NO C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     noc = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'C' or my_string[i] != 'c'):
+        if (my_string[i] != 'C' and my_string[i] != 'c'):
             noc += my_string[i]
     return noc
