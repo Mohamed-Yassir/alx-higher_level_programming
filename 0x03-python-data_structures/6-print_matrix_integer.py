2830d64de99edc09f4c21f7c8e7ba153b49112b9
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-03-19 12:41:54 -0700

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for mat in matrix:
+        if len(mat) == 0:
+            print()
+        for i in range(len(mat)):
+            print("{:d}".format(mat[i]),
+                    end="\n" if i is len(mat) - 1 else " "
