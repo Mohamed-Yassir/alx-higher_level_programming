29e9a383ad8afca9d0e2c58e924a3e466a01acde
Mohamed-Yassir.Mohamed <Mohamed-Yassir.mo.gafar25@gmail.com>
2024-08-08 23:08:29 -0700

    JavaScript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//script to display javascript is amazing
-const myVar = "JavaScript is amazing";
+// script to display javascript is amazing
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
