e706df2a91369847ab27541b62ded6fff08438af
Mohamed-Yassir.Mohamed <Mohamed-Yassir.mo.gafar25@gmail.com>
2024-08-09 23:11:16 -0700

    m

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 // print addition of 2 numbers
+const first = process.argv[2];
+const sec = process.argv[3];
 function add (a, b) {
-  return parseInt(a) + parseInt(b);
+  return a + b;
 }
-console.log(add(process.argv[2], process.argv[3]));
+const a = parseInt(first);
+const b = parseInt(sec);
+console.log(add(a, b));
