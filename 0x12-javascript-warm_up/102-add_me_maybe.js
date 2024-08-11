a3377cea1b0d70e11ff83760a312e0c9c80dcfe3
Mohamed-Yassir.Mohamed <Mohamed-Yassir.mo.gafar25@gmail.com>
2024-08-10 22:48:02 -0700

    maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // function that increment
-exports.addMe = function (number, theFunction) {
+exports.addMeMaybe = function (number, theFunction) {
   number++;
   theFunction(number);
 };
