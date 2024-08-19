2442da9516ad15dc7fade175eda00ef9becb3e9f
root <Mohamed-Yassir.mo.gafar25@gmail.com>
2024-08-18 23:36:04 -0700

    square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""Square Module"""
 
 class Square:
     """Defines a Square."""
+
     def __init__(self, size=0):
         """Constructor.
-
         Args:
+
             size: Length of a side of the square.
         """
         self.size = size
